2c4f401b64b300eda9505297909e01dc3b0439b5
Rahul Pandya <rahul25796@gmail.com>
2020-02-09 17:43:13 -0500

    initial commit

---
+++ src/main/java/com/teach/me/app/TeachMeAppApplication.java
@@ -0,0 +1,13 @@
+package com.teach.me.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachMeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachMeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teach/me/app/TeachMeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teach.me.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachMeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
