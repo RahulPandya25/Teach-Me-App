9fc23d20f690c710e45b3c57889ed1fc8dd08b55
Richa <gupta14h@uwindsor.ca>
2020-03-19 00:11:38 -0400

    Added unit test package

---
+++ src/main/java/com/UnitTests/QuestionControllerTest.java
@@ -0,0 +1,14 @@
+package com.UnitTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionControllerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+}
